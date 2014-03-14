631e457c3f4b8460439116d09b1df22ab611f148
K-RKidd <kr248196@muhlenberg.edu>
2014-03-14 10:00:36 -0400

    Initial Commit

---
+++ Quiz5/Quiz5AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  Quiz5AppDelegate.h
+//  Quiz5
+//
+//  Created by Krystle on 3/14/14.
+//  Copyright (c) 2014 Krystle Kidd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Quiz5AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Quiz5/Quiz5ViewController.h
@@ -0,0 +1,13 @@
+//
+//  Quiz5ViewController.h
+//  Quiz5
+//
+//  Created by Krystle on 3/14/14.
+//  Copyright (c) 2014 Krystle Kidd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Quiz5ViewController : UIViewController
+
+@end
